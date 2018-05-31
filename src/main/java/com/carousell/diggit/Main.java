674e3f5ea2ee18cb99e4a570dda44c02f2b9d707
Moath Almasri <moath.almasri91@gmail.com>
2018-05-31 18:14:46 +0400

    Develop Branch - Initial commit

---
+++ src/main/java/com/carousell/diggit/Main.java
@@ -0,0 +1,12 @@
+package com.carousell.diggit;
+
+/**
+ * This class is the starting point of the application.
+ *
+ * @author Moath
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
